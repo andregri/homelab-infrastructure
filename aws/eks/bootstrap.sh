d6667ce7a16d3e3f9263ebe318594a54419d9915
andregri <andrea.grillo96@live.com>
2023-12-25 17:38:50 +0100

    update eks lab

--- aws/eks/bootstrap.sh
+++ aws/eks/bootstrap.sh
@@ -29,10 +29,13 @@ eksctl version
 # Create the cluster
 eksctl create cluster --name dev --region us-east-1 --zones=us-east-1a,us-east-1b,us-east-1d --nodegroup-name standard-workers --node-type t3.medium --nodes 3 --nodes-min 1 --nodes-max 4 --managed
 
-# 
+# update kubeconfig
 aws eks update-kubeconfig --region us-east-1 --name dev
 
-#
+# install a demo app
 sudo yum install -y git
 git clone https://github.com/ACloudGuru-Resources/Course_EKS-Basics
 cd Course_EKS-Basics
+
+# delete the cluster
+eksctl delete cluster dev
